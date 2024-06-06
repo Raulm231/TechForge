df2bee1021ab5778601ba04d36e339629a45cee6
Raulm231 <sraulmiguel231@gmail.com>
2024-06-06 11:44:11 -0300

    Adicionado Bootstrap

--- js2.js
+++ js2.js
@@ -1,10 +1,12 @@
-Numero= 8
+aplicacao = 1000;
+taxaJuros = 7%1000;
+periodo = 16;
+jurosTotal = 0;
 
-function verificaNumber(Numero) {
-    if (Numero % 2 == 0) {
-        console.log("Numero é Par: ", Numero)
-    }
-        else {
-        console.log("Numero é Impar: ", Numero)
-        }
+for (i=0;i<periodo;i++) {
+    jurosSimples=aplicacao * taxaJuros/100
+    console.log("Este é o juros simples mensal" + jurosSimples)
+    jurosTotal=jurosTotal + jurosSimples
+    aplicacao+=jurosSimples
 }
+console.log("Este é o Valor final do Juros: " + jurosTotal); 
